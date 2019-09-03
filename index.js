b20cf62a6cf3e838e2ec8efd7adb8752cf8418da
Yahub <johndoe@example.com>
2019-09-03 20:34:30 +0300

    2

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ mongoose.connect(
 );
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => res.send("api running"));
 app.use("/api", authRoute);
 app.use("/api/notes", noteRoute);
